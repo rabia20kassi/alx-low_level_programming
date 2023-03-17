4944b757b14345afe87f5a29c4ac3b124fbba841
rabia20kassi <rabiaakassi@gmail.com>
2023-03-16 17:34:36 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
-* main - A program that prints a line with puts function
-* Return : 0 (Succes)
-*/
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
+
 /**
-* main - Prints out a string to stdout.
-* Return : 0 (Succes)
-*/
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s) \n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s) \n", (unsigned long)sizeof(c));
-printf("Size of a clong long int: %lu byte(s) \n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s) \n", (unsigned long)sizeof(f));
-return (0);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
