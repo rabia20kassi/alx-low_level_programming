7558e4dfbdf915f7ffe913317f79d11478b8f10a
rabia20kassi <rabiaakassi@gmail.com>
2023-03-21 16:48:49 -0700

    3-islower.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,18 @@
 #include "main.h"
+
 /**
- * print_alphabet 
- * Return: void
- */
+  * _islower - Checks for lowercase character
+  * @c: The character to be checked
+  * Return: 1 for lowercase character or 0 for anything else
+  */
+
 void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(i);
+	_putchar(i);
 	}
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * _islower function that checks for lowercase character
- *
- * Return: 1 if true, 0 if false
- *
- */
+  * _islower - Checks for lowercase character
+  * @c: The character to be checked
+  * Return: 1 for lowercase character or 0 for anything else
+  */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return (1);
+	return (1);
 	}
 	return (0);
 }
