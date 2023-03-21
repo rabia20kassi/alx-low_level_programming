a81bab2aba980b1ca5f304c48b27e2cee3fada1f
rabia20kassi <rabiaakassi@gmail.com>
2023-03-21 14:42:11 -0700

    Task C - Functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * main - program that prints _putchar
+ *
+ * Return: 0 if no error
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
