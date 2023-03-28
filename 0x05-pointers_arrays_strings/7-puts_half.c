5870583d1787a4ed58d557c40310bb179e984e9c
rabia20kassi <rabiaakassi@gmail.com>
2023-03-27 18:42:52 -0700

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to print
+ */
+
+void puts_half(char *str)
+{
+	int idx;
+	int len = _strlen(str);
+
+	/* find the index to start depending on even/odd amount of strlen */
+	if (len % 2 != 0)
+		idx = (len / 2) + 1;
+	else
+		idx = (len / 2);
+
+	while (idx < len)
+	{
+		_putchar(*(str + idx));
+		idx++;
+	}
+	_putchar('\n');
+}
