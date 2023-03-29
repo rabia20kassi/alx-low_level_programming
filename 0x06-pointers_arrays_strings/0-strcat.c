7ec6d8563bfa010834c4ea43ee5504f54439b5ce
rabia20kassi <rabiaakassi@gmail.com>
2023-03-29 16:28:19 -0700

    Task C

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * _strcat - Concatenates two strings
  * @dest: Pointer to destination string
@@ -6,24 +6,21 @@
  *
  * Return: Pointer to destination string
  */
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-        int i = 0, j = 0;
+	int i = 0, j = 0;
 
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
-
-        while (src[j] != '\0')
-        {
-                dest[i] = src[j];
-                i++;
-                j++;
-        }
-
-        dest[i] = '\0';
-
-        return (dest);
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
