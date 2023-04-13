b45717e0f41922a497f30ff208b86b14cbd405f7
rabia20kassi <rabiaakassi@gmail.com>
2023-04-12 18:01:54 -0700

    #advanced

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - Entry point
+ * @ac: int
+ * @av: Character
+ * Description: concatenates all the arguments of your program
+ * Return: char
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, len = 0, index = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			len++;
+		}
+		len++;
+	}
+	len++;
+	str = malloc(len * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[index++] = av[i][j];
+		}
+		str[index++] = '\n';
+	}
+	str[index] = '\0';
+	return (str);
+}
