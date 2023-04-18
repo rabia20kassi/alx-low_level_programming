44b6d0c361b26d90a12e01b0382127febd4a461c
rabia20kassi <rabiaakassi@gmail.com>
2023-04-18 02:40:13 +0000

    struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs
+ * @d: dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
