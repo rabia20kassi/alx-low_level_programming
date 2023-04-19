6fad6a7b10d9fdb29095f185966d4ee0dc34ac58
rabia20kassi <rabiaakassi@gmail.com>
2023-04-19 22:16:03 +0000

    task1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
 
 	if (array == NULL || action == NULL)
 		return;
 
-	for (i = 0; i <= size - 1; i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
