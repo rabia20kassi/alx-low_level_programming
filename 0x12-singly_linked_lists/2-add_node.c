3f87107f3ce8a10961a06af6a3420b45b6831243
rabia20kassi <rabiaakassi@gmail.com>
2023-04-28 12:50:39 +0100

    task-2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: Pointer to the head of the list_t list
+ * @str: String added to the list_t list
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *b;
+	int len = 0;
+	char *copy;
+	
+	copy = strdup(str);
+	b =(list_t*) malloc(sizeof(list_t));
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+	b->str = copy;
+	if (b->str == NULL)
+	{
+		free(b);
+		return (NULL);
+	}
+	while (str[len])
+		len++;
+	b->next = *head;
+	b->len = len;
+	*head = b;
+	return (b);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
 #ifndef LIST_H
 #define List_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -19,6 +20,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
