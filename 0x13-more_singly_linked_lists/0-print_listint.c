bb61759e29d0f2fbd47df9bfe0f6229598c2f788
rabia20kassi <rabiaakassi@gmail.com>
2023-05-02 00:16:45 +0100

    task-0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
-	const listint_t *temp;
 
-	temp = h;
 	if (h == NULL)
 		return (0);
-	while (temp != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		nodes++;
 	}
 	return (nodes);
