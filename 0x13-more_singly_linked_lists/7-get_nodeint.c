20061b463c2441c8c9e1d47348b94be8b6ea2a71
rabia20kassi <rabiaakassi@gmail.com>
2023-05-02 21:53:32 +0100

    task-7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int i;
 
+	if (!head)
+		return (NULL);
 	temp = head;
 	for (i = 0; i < index && temp; i++)
 	{
