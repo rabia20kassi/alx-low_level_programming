4b5fa7a63d123d9962eb235c2f9f83ef85100cfe
rabia20kassi <rabiaakassi@gmail.com>
2023-05-02 15:04:39 +0100

    task-5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (*head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-	head = NULL;
+	*head = NULL;
 }
